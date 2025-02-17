5e38607838bd407f213a822d2d3cea615c0c159a
AgentMeap <huudatnguyen15@gmail.com>
2025-02-17 12:49:34 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import ReactDOM from "react-dom/client";
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from '@pages/RootLayout';
+import ModalProvider from '@context/ModalProvider';
+import HomePage from '@pages/HomePage';
+import { ThemeProvider } from "@mui/material";
+import theme from '@configs/muiConfig';
+import RegisterPage from "@pages/RegisterPage";
+
+
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },{
+        path: '/register',
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+  <ModalProvider>
+    <RouterProvider router={router} />
+  </ModalProvider>
+  </ThemeProvider>
+
+);
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,10 @@
+const RegisterPage = () => {
+  return (
+    <div>
+      <form action="">
+        
+      </form>
+    </div>
+  );
+};
+export default RegisterPage;

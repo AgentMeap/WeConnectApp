e2f5041f44ebf9a818bb79984d2ef994fc00b6f6
AgentMeap <huudatnguyen15@gmail.com>
2025-02-18 21:34:19 +0700

    add endpoint login page

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,31 +1,66 @@
 import FormField from '@components/FormField';
 import TextInput from '@components/FormInputs/TextInput';
-import { Button } from '@mui/material';
-import { login } from '@redux/slices/authSlice';
+import { Button, CircularProgress } from '@mui/material';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useLoginMutation } from '@services/rootApi';
+import { openSnackbar } from '@redux/slices/snackbarSlice';
 
 const LoginPage = () => {
-  const { control } = useForm();
+  const [login, { data = {}, isLoading, error, isSuccess, isError }] =
+    useLoginMutation();
+
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  //test action login
+  const formSchema = yup.object().shape({
+    email: yup
+      .string()
+      .matches(
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        'Email is not valid',
+      )
+      .required(),
+    password: yup.string().required(),
+  });
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  function onSubmit(formData) {
+    console.log({ formData });
+    login(formData);
+  }
+
   useEffect(() => {
-    dispatch(
-      login({ accessToken: 'accessToken', refreshToken: 'refreshToken' }),
-    );
-  }, [dispatch]);
+    if (isError) {
+      dispatch(openSnackbar({ type: 'error', message: error?.data?.message }));
+    }
+    if (isSuccess) {
+      dispatch(openSnackbar({ message: data?.message }));
+      navigate('/verify-otp');
+    }
+  }, [isError, error, dispatch, isSuccess, data, navigate]);
+
   return (
     <div>
       <p className="mb-5 text-center text-2xl font-bold">Login</p>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
         <FormField
           name="email"
           label="Email"
           control={control}
           Component={TextInput}
+          error={errors['email']}
         />
         <FormField
           name="password"
@@ -33,8 +68,12 @@ const LoginPage = () => {
           control={control}
           type="password"
           Component={TextInput}
+          error={errors['password']}
         />
         <Button type="submit" variant="contained">
+          {isLoading && (
+            <CircularProgress size={16} className="mr-2"></CircularProgress>
+          )}
           Sign In
         </Button>
       </form>
--- src/services/rootApi.js
+++ src/services/rootApi.js
@@ -18,8 +18,17 @@ export const rootApi = createApi({
           };
         },
       }),
+      login: builder.mutation({
+        query: ({ email, password }) => {
+          return {
+            url: '/login',
+            body: { email, password },
+            method: 'POST',
+          };
+        },
+      }),
     };
   },
 });
 
-export const { useRegisterMutation } = rootApi;
+export const { useRegisterMutation, useLoginMutation } = rootApi;

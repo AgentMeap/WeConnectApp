24c1874ef5a1bfd2a8c9fda09968051936294eaf
AgentMeap <huudatnguyen15@gmail.com>
2025-02-19 17:07:26 +0700

    add header

--- src/pages/ProtectedLayout.jsx
+++ src/pages/ProtectedLayout.jsx
@@ -1,8 +1,9 @@
+import Header from '@components/Header';
 import { saveUserInfo } from '@redux/slices/authSlice';
 import { useGetAuthUserQuery } from '@services/rootApi';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Link, Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedLayout = () => {
   const response = useGetAuthUserQuery();
@@ -34,8 +35,7 @@ const ProtectedLayout = () => {
 
   return (
     <div>
-      <Link to="/">Home Page</Link>
-      <Link to="/message">Message Page</Link>
+      <Header />
       <Outlet />
     </div>
   );
